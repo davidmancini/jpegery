3f08e0b81802c248718569edab272939fbb40666
Jacob Findley <jfindley2@cnm.edu>
2016-01-26 13:04:21 -0700

    Added system-goals.php

--- personas/johndoe.php
+++ personas/johndoe.php
@@ -4,6 +4,13 @@
 		<meta charset="utf-8" />
 	</head>
 	<body>
-
+		<div>
+			<p><strong>Name:</strong> John Doe</p>
+			<p><strong>Age:</strong> 23</p>
+			<p><strong>Job:</strong> Graduate Student, TA</p>
+			<p><strong>Goal:</strong> John has a casual interest in art, sketches of architecture in particular. He wishes to share his work for advice, inspiration, and praise. As such, he has posted content on jpegery three times, and is visiting the site to read any comments on his work.</p>
+			<p><strong>History:</strong> John was born into a middle-class family. He has one younger brother who is in his senior year of high-school. Both of his parents are employed. He majored in History two years ago and is currently working on his masters. He is somewhat bilingual, being relatively proficient with Russian.</p>
+			<p><strong></strong></p>
+		</div>
 	</body>
 </html>
