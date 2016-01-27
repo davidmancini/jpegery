af0ee415fc3d225ba56133a641ff3712187ff915
David Mancini <mancini.david@gmail.com>
2016-01-27 14:28:57 -0700

    Added head tag file to be imported to HTML files for Bootstrap.  Changed index.html to include Bootstrap.

--- epic/index.php
+++ epic/index.php
@@ -3,20 +3,26 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title>The Index</title>
+		<!--Include Bootstrap Head Tags -->
+		<? include_once ("../lib/head-tags.html"); ?>
+		<link rel="stylesheet" href="../css/epicstyles.css">
 	</head>
 	<body>
-		<div>
-			<h1>Links:</h1>
-			<a href="system-goals.php">The System Goals</a><br/>
-			<a href="use-cases.php">The Use Cases</a><br/>
-			<a href="user-stories.php">The User Stories</a><br/>
-			<a href="personas/johndoe.php">Persona 1: John Doe</a><br/>
-			<a href="data-design/conceptual-model.php">The Conceptual Model</a><br/>
-			<a href="data-design/erd.php">The ERD</a><br/>
-		</div>
-		<div>
-			<h1>Executive Summary:</h1>
-			<p>A website wherein users post and review media.</p>
+			<div class="container">
+				<h1>jpegery Epic</h1>
+				<div>
+				<h2>Links:</h2>
+				<a href="system-goals.php">The System Goals</a><br/>
+				<a href="use-cases.php">The Use Cases</a><br/>
+				<a href="user-stories.php">The User Stories</a><br/>
+				<a href="personas/johndoe.php">Persona 1: John Doe</a><br/>
+				<a href="data-design/conceptual-model.php">The Conceptual Model</a><br/>
+				<a href="data-design/erd.php">The ERD</a><br/>
+			</div>
+			<div>
+				<h2>Executive Summary:</h2>
+				<p>A website wherein users post and review media.</p>
+			</div>
 		</div>
 	</body>
 </html>
