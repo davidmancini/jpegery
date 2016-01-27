51e918fa4785312163797999501a14f627d6d478
Jacob Findley <jfindley2@cnm.edu>
2016-01-27 16:06:09 -0700

    Updated use-cases to include the first use case

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -5,6 +5,20 @@
 		<title>Use Cases</title>
 	</head>
 	<body>
+		<div>
+			<h2>Uploading Media</h2>
+			<ul>
+				<li>User: Click on the upload icon</li>
+				<li>Site: Open the upload window and ask user which file they would like to upload</li>
+				<li>User: Select appropriate file</li>
+				<li>User: Click Upload</li>
+				<li>Site: Display uploaded file</li>
+				<li>Site: Open text field and prompt user to add a text description about their content</li>
+				<li>User: Write description and add tag, or leave text field blank</li>
+				<li>User: Click submit</li>
+				<li>Site: Display completed post</li>
 
+			</ul>
+		</div>
 	</body>
 </html>
