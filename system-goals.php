044c466dfcf093d4568eecbdd48d037a09b2a16e
Jacob Findley <jfindley2@cnm.edu>
2016-01-27 12:44:13 -0700

    Added the basic html skeleton to all of the relevant files.

--- system-goals.php
+++ system-goals.php
@@ -5,7 +5,7 @@
 	</head>
 	<body>
 		<div>
-			<p></p>
+			<p>By using this site, the end user will be able to submit images for others to see, view images that others have submitted, comment on posted images, and "favorite" the creators of images they like.</p>
 		</div>
 	</body>
 </html>
