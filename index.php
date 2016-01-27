356a80a0c93641f15f46217168155ae5585f059f
Jacob Findley <jfindley2@cnm.edu>
2016-01-27 10:53:57 -0700

    Updated index to include current links.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+
+	</head>
+	<body>
+		<div>
+			<a href="system-goals.php">The System Goals</a>
+			<a href="use-cases.php">The Use Cases</a>
+			<a href="user-stories.php">The User Stories</a>
+			<a href="./personas/johndoe.php">Persona 1: John Doe</a>
+			<a href="./data-design/conceptual-model.php">The Conceptual Model</a>
+			<a href="./data-design/erd.php">The ERD</a>
+		</div>
+	</body>
+</html>
