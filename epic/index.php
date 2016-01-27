3ccef787b65dcc6ca4607a6cdf0133f41cef34f5
David Mancini <mancini.david@gmail.com>
2016-01-27 16:16:19 -0700

    Added persona content.

--- epic/index.php
+++ epic/index.php
@@ -23,28 +23,35 @@
 				<div class="row">
 					<div class="col-md-6 personaborder">
 						<img src="../images/persona1.jpg" alt="Persona 1" class="img-circle img-responsive center-block" height="300" width="300"><br>
-						<p class="text-center">
-							<strong>Jason Holmes, 23</strong><br>Graduate Student, TA<br><br>
-							<strong>User Type:</strong> Non-professional, irregular poster.<br>
-							<strong>Goal:</strong> John has a casual interest in art, sketches of architecture in particular. He wishes to share his work for advice, inspiration, and praise. As such, he has posted content on jpegery three times, and is visiting the site to read any comments on his work.<br>
-							<strong>History:</strong> John was born into a middle-class family. He has one younger brother who is in his senior year of high-school. Both of his parents are employed. He majored in History two years ago and is currently working on his masters. He is somewhat bilingual, being relatively proficient with Russian. His parents probably thought they were very clever and ironic when they were naming him.
+						<p class="text-center"><strong>Jason Holmes, 23</strong><br>Graduate Student, TA</p><br>
+							<strong>User Type: </strong>Non-professional, irregular poster.<br><br>
+							<strong>Goal: </strong>John has a casual interest in art, sketches of architecture in particular. He wishes to share his work for advice, inspiration, and praise. As such, he has posted content on jpegery three times, and is visiting the site to read any comments on his work.<br><br>
+							<strong>History: </strong>John was born into a middle-class family. He has one younger brother who is in his senior year of high-school. Both of his parents are employed. He majored in History two years ago and is currently working on his masters. He is somewhat bilingual, being relatively proficient with Russian. His parents probably thought they were very clever and ironic when they were naming him.
 
-						</p>
 
 					</div>
 					<div class="col-md-6 personaborder">
 						<img src="../images/persona2.jpg" alt="Persona 2" class="img-circle img-responsive center-block"><br>
-						<p class="text-center">Persona 2</p>
+						<p class="text-center"><strong>Dana McNeill, 26</strong><br>Accounting</p><br>
+						<strong>User Type: </strong>Non-professional, but posts regularly<br><br>
+						<strong>Goal: </strong>Following the advice of a speaker at her old University, Dana has taken to creating an image diary, drawing something that she saw or experienced every day in ink. She has posted them on jpegery every day for a bit more than a year, often providing comments describing the context of the images. This is primarily a hobby for her, and other than a vague satisfaction in how much her art has improved and how she hasn’t missed a day, she has no real goals or agendas in her drawings.<br><br>
+						<strong>History: </strong>Dana grew up a military brat, living on a base for most of her childhood. She’s very self sufficient, though this is less something she intentionally pursued and more something that just happened. She’s aggressive in the stock market.
 					</div>
 				</div><br>
 				<div class="row">
 					<div class="col-md-6 personaborder">
 						<img src="../images/persona3.jpg" alt="Persona 3" class="img-circle img-responsive center-block"><br>
-						<p class="text-center">Persona 3</p>
+						<p class="text-center"><strong>Jessica Kelso, 14</strong><br>Student</p><br>
+						<strong>User Type: </strong>Commenter and Viewer<br><br>
+						<strong>Goal: </strong>Jessica likes looking at her friends’ photography.  She wants to be a photographer, but she doesn’t have a camera yet.  She uses other social media for sharing her own status and photos (from her phone), but uses jpegery to get inspiration from famous photographers.<br><br>
+						<strong>History: </strong>Jessica is in middle school; her life is consumed by her schoolwork but mostly by her friends and her social life.  Though she has access to a desktop computer, she accesses the Internet primarily via her smartphone.
 					</div>
 					<div class="col-md-6 personaborder">
 						<img src="../images/persona4.jpg" alt="Persona 4" class="img-circle img-responsive center-block"><br>
-						<p class="text-center">Persona 4</p>
+						<p class="text-center"><strong>Chad Polumbaum, 47</strong><br>Artist</p><br>
+						<strong>User Type: </strong>Professional artist looking to advertise himself<br><br>
+						<strong>Goal: </strong>Chad is accomplished in digital media, abstract portraits in particular. He is semi-regularly asked to take commissions, and has been posting portraits (Which he created for practice, rather than profit) or other images on jpeger an average of once a month for the past two years.<br><br>
+						<strong>History: </strong>Chad is the father of two boys, ages ten and twelve. He left his job as a design consultant at Generic Inc. eleven years ago to raise his children, as his wife had a better salary and superior retirement benefits. He has strong opinions about politics.
 					</div>
 				</div>
 <br><br>
