030650742b7a892cc399341f65d5209ddd583fc8
Jacob Findley <jfindley2@cnm.edu>
2016-01-27 12:50:46 -0700

    Updated User-stories.

--- user-stories.php
+++ user-stories.php
@@ -4,6 +4,22 @@
 		<meta charset="utf-8"/>
 	</head>
 	<body>
-
+		<div>
+			<p>
+				"As a user, I want to view a user's image."
+				"As a user, I want to comment on a user's image."
+				"As a user, I want to upvote or downvote a user's image."
+				"As a user, I want to view the top images."
+				"As a user, I want to post images on the site."
+				"As a user, I want to create an account."
+				"As a user, I want to update my profile."
+				"As an administrator, I want to ban a user."
+				"As an administrator, I want to suspend a user."
+				"As an administrator, I want to warn a user."
+				"As an administrator, I want to update the site's news, such that I can communicate to users."
+				"As an administrator, I want to remove a user's image.
+				"As an administrator, I want to remove a user's comment."
+			</p>
+		</div>
 	</body>
 </html>
