8ee305f1596d2f2e1405fe6d10435af3a133dc10
zleyba <mtvzach@gmail.com>
2016-01-27 14:13:34 -0700

    added css and persona php

---
+++ epic/personas/persona1.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+
+		<link href='https://fonts.googleapis.com/css?family=Kanit:400,500italic,100italic,200,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+		<link type="text/css" href="https://bootcamp-coders.cnm.edu/~zleyba/clinstrument/css/style.css" rel="stylesheet" />
+
+		<title>Craigslist Data Design</title>
+	</head>
+	<body>
+		<header>
+			<h1>j p e g e r y</h1>
+			<h2>EPIC</h2>
+		</header>
+
+		<div class="persona">
+			<h1>p e r s o n a</h1>
+
+
+			<div class="profile">
+				<h3>Meredith Offner</h3>
+				<h4>“I can’t wait to share the world of music with my son.”</h4>
+				<img src="https://bootcamp-coders.cnm.edu/~zleyba/clinstrument/img/meredith.JPG" alt="Meredith Offner">
+			</div>
+
+			<div class="descript">
+				<h3>Musical Mother</h3>
+				<p>Meredith has never been a fan of online shopping, but has little choice as there are not
+					many places that sell music equipment in her town. When making used purchases she prefers
+					to deal in person so that she can inspect the purchase and avoid waiting for shipping.
+				</p>
+			</div>
+			<div class="needs">
+				<h3>Needs</h3>
+				<ul>
+					<li>A way to be able to sort through all the instruments for sale in her area
+					<li>Clear description of the instrument she’s looking at including an image
+					<li>Clearly marked price and an easy way to contact the seller
+
+				</ul>
+
+
+			</div>
+
+			<br>
+
+			<div class="goals">
+				<p>Meredith Offner is a 35 year-old musician and single mother. She is looking for a new instrument for
+					her 8 year-old son who has shown interest in learning how to play music. Money is not necessarily
+					tight in her household, but she’s not one to spend more money than she needs to. She wants a quality
+					instrument that her son can use to test the waters of musicianship but also something that can be played
+					a comfortable volume and not annoy neighbors in their apartment complex. With quality and volume in mind,
+					she has set out looking for a used keyboard or acoustic guitar.</p>
+
+			</div>
+
+		</div>
+		<br>
+		<div class="content">
+			<h3>u s e &nbsp;c a s e</h3>
+
+			<ol>
+				<li>Meredith logs on to craigslist.org
+				<li>Selects the closest city
+				<li>After applying a price filter she begins to peruse the listing gallery
+				<li>Finds a listing for a suitable acoustic bass
+				<li>Contacts seller to complete transaction
+			</ol>
+
+			<h3>Alt c a s e</h3>
+
+			<ol>
+				<li>Meredith conducts a Google search for “used instruments *name of her town*”
+				<li>Discovers craigslist page for the town closest to her
+				<li>Conducts a search for acoustic guitar in the search field
+				<li>Finds listing for a suitable acoustic bass
+				<li>Contacts seller to complete transaction
+
+			</ol>
+
+	</body>
+</html>
