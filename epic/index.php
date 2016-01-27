1bbc7dd9ac2c1f07729222755ac3de47fe01d05d
David Mancini <mancini.david@gmail.com>
2016-01-27 14:37:38 -0700

    Updated for Bootstrap

--- epic/index.php
+++ epic/index.php
@@ -2,16 +2,22 @@
 	<html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>The Index</title>
+		<title>jpegery Epic</title>
 		<!--Include Bootstrap Head Tags -->
 		<? include_once ("../lib/head-tags.html"); ?>
-		<link rel="stylesheet" href="../css/epicstyles.css">
+<!--		<link rel="stylesheet" href="../css/epicstyles.css">-->
 	</head>
 	<body>
 			<div class="container">
-				<h1>jpegery Epic</h1>
+
+				<h1 class="page-header">jpegery Epic</h1>
 				<div>
-				<h2>Links:</h2>
+					<h2>Executive Summary</h2>
+					<p>A website wherein users post and review media.</p>
+				</div>
+
+				<div>
+				<h2>Links</h2>
 				<a href="system-goals.php">The System Goals</a><br/>
 				<a href="use-cases.php">The Use Cases</a><br/>
 				<a href="user-stories.php">The User Stories</a><br/>
@@ -19,10 +25,7 @@
 				<a href="data-design/conceptual-model.php">The Conceptual Model</a><br/>
 				<a href="data-design/erd.php">The ERD</a><br/>
 			</div>
-			<div>
-				<h2>Executive Summary:</h2>
-				<p>A website wherein users post and review media.</p>
-			</div>
+
 		</div>
 	</body>
 </html>
