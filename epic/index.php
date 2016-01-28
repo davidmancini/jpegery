c5ecc1b0c75204f53f5edf9ba8242930e21026e9
David Mancini <mancini.david@gmail.com>
2016-01-28 09:17:39 -0700

    Added CSS to make persona rows equal height.

--- epic/index.php
+++ epic/index.php
@@ -12,7 +12,7 @@
 
 				<h1 class="page-header">jpegery Epic</h1>
 
-				<div class="row">
+				<div class="row" >
 					<div class="col-md-12">
 						<h3>Executive Summary</h3>
 						<p>jpegery is a lightweight, web-based, image sharing platform with a focus on original content.
@@ -22,7 +22,7 @@
 				</div>
 
 				<h3>Personas</h3><br>
-				<div class="row">
+				<div class="row row-eq-height">
 					<div class="col-md-6 personaborder">
 						<div class="content-tile">
 							<img src="../images/persona1.jpg" alt="Persona 1" class="img-circle img-responsive center-block" height="300" width="300"><br>
@@ -34,23 +34,24 @@
 
 					</div>
 					<div class="col-md-6 personaborder">
-						<img src="../images/persona2.jpg" alt="Persona 2" class="img-circle img-responsive center-block"><br>
+						<img src="../images/persona2.jpg" alt="Persona 2" class="img-circle img-responsive center-block" height="300" width="300"><br>
 						<p class="text-center"><strong>Dana McNeill, 26</strong><br>Accounting</p><br>
 						<strong>User Type: </strong>Non-professional, but posts regularly<br><br>
 						<strong>Goal: </strong>Following the advice of a speaker at her old University, Dana has taken to creating an image diary, drawing something that she saw or experienced every day in ink. She has posted them on jpegery every day for a bit more than a year, often providing comments describing the context of the images. This is primarily a hobby for her, and other than a vague satisfaction in how much her art has improved and how she hasn’t missed a day, she has no real goals or agendas in her drawings.<br><br>
 						<strong>History: </strong>Dana grew up a military brat, living on a base for most of her childhood. She’s very self sufficient, though this is less something she intentionally pursued and more something that just happened. She’s aggressive in the stock market.
 					</div>
-				</div><br>
-				<div class="row">
+				</div>
+
+				<div class="row row-eq-height">
 					<div class="col-md-6 personaborder">
-						<img src="../images/persona3.jpg" alt="Persona 3" class="img-circle img-responsive center-block"><br>
+						<img src="../images/persona3.jpg" alt="Persona 3" class="img-circle img-responsive center-block" height="300" width="300"><br>
 						<p class="text-center"><strong>Jessica Kelso, 14</strong><br>Student</p><br>
 						<strong>User Type: </strong>Commenter and Viewer<br><br>
 						<strong>Goal: </strong>Jessica likes looking at her friends’ photography.  She wants to be a photographer, but she doesn’t have a camera yet.  She uses other social media for sharing her own status and photos (from her phone), but uses jpegery to get inspiration from famous photographers.<br><br>
 						<strong>History: </strong>Jessica is in middle school; her life is consumed by her schoolwork but mostly by her friends and her social life.  Though she has access to a desktop computer, she accesses the Internet primarily via her smartphone.
 					</div>
 					<div class="col-md-6 personaborder">
-						<img src="../images/persona4.jpg" alt="Persona 4" class="img-circle img-responsive center-block"><br>
+						<img src="../images/persona4.jpg" alt="Persona 4" class="img-circle img-responsive center-block" height="300" width="300"><br>
 						<p class="text-center"><strong>Chad Polumbaum, 47</strong><br>Artist</p><br>
 						<strong>User Type: </strong>Professional artist looking to advertise himself<br><br>
 						<strong>Goal: </strong>Chad is accomplished in digital media, abstract portraits in particular. He is semi-regularly asked to take commissions, and has been posting portraits (Which he created for practice, rather than profit) or other images on jpeger an average of once a month for the past two years.<br><br>
