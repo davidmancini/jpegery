2b87944b4603fd1735912db8675553ef4c8e2c25
David Mancini <mancini.david@gmail.com>
2016-01-28 12:30:35 -0700

    Removed info from Personas.  Will move to different pages.

--- epic/index.php
+++ epic/index.php
@@ -15,8 +15,8 @@
 				<div class="row" >
 					<div class="col-md-12">
 						<h3>Executive Summary</h3>
-						<p>jpegery is a lightweight, web-based, image sharing platform with a focus on original content.
-							Our primary interest is giving credit to individuals who create compelling, unique, and viral visual content.  jpegery will provide users two primary methods to discover images that appeal to them. First they can search through hashtags and users, then they can follow the tags and accounts they like the most. While jpegery may not be able to stop content theft, our goal is to make it easier for creators to get the recognition they deserve.
+						<p>jpegery is a web-based, image-sharing platform.  Our goals are to make it easier for creators of original content to get the recognition they deserve and to make it easier for users to discover new images.<br>
+							jpegery will provide users with two primary methods of discovering images that appeal to them. First, they can search through hashtags and users.  Then, they can follow the tags and accounts they like the most.
 						</p>
 					</div>
 				</div>
@@ -24,41 +24,23 @@
 				<h3>Personas</h3><br>
 				<div class="row row-eq-height">
 					<div class="col-md-6 personaborder">
-						<div class="content-tile">
-							<img src="../images/persona1.jpg" alt="Persona 1" class="img-circle img-responsive center-block" height="300" width="300"><br>
+							<img src="../images/persona1.jpg" alt="Persona 1" class="img-circle img-responsive center-block"<br>
 							<p class="text-center"><strong>Jason Holmes, 23</strong><br>Graduate Student, TA</p><br>
-							<strong>User Type: </strong>Non-professional, irregular poster.<br><br>
-							<strong>Goal: </strong>John has a casual interest in art, sketches of architecture in particular. He wishes to share his work for advice, inspiration, and praise. As such, he has posted content on jpegery three times, and is visiting the site to read any comments on his work.<br><br>
-							<strong>History: </strong>John was born into a middle-class family. He has one younger brother who is in his senior year of high-school. Both of his parents are employed. He majored in History two years ago and is currently working on his masters. He is somewhat bilingual, being relatively proficient with Russian. His parents probably thought they were very clever and ironic when they were naming him.
+							Infrequent poster interested in his images' comments.<br><br>
 						</div>
-
-					</div>
 					<div class="col-md-6 personaborder">
-						<img src="../images/persona2.jpg" alt="Persona 2" class="img-circle img-responsive center-block" height="300" width="300"><br>
+						<img src="../images/persona2.jpg" alt="Persona 2" class="img-circle img-responsive center-block"><br>
 						<p class="text-center"><strong>Dana McNeill, 26</strong><br>Accounting</p><br>
-						<strong>User Type: </strong>Non-professional, but posts regularly<br><br>
-						<strong>Goal: </strong>Following the advice of a speaker at her old University, Dana has taken to creating an image diary, drawing something that she saw or experienced every day in ink. She has posted them on jpegery every day for a bit more than a year, often providing comments describing the context of the images. This is primarily a hobby for her, and other than a vague satisfaction in how much her art has improved and how she hasn’t missed a day, she has no real goals or agendas in her drawings.<br><br>
-						<strong>History: </strong>Dana grew up a military brat, living on a base for most of her childhood. She’s very self sufficient, though this is less something she intentionally pursued and more something that just happened. She’s aggressive in the stock market.
+						Frequent poster interested in the social aspect.<br><br>
+					</div>
+					<div class="col-md-6 personaborder">
+						<img src="../images/persona3.jpg" alt="Persona 3" class="img-circle img-responsive center-block"><br>
+						<p class="text-center"><strong>Jessica Kelso, 14</strong><br>Student</p><br>
+						Frequent Commenter and Viewer, but does not post.<br><br>
 					</div>
 				</div>
 
-				<div class="row row-eq-height">
-					<div class="col-md-6 personaborder">
-						<img src="../images/persona3.jpg" alt="Persona 3" class="img-circle img-responsive center-block" height="300" width="300"><br>
-						<p class="text-center"><strong>Jessica Kelso, 14</strong><br>Student</p><br>
-						<strong>User Type: </strong>Commenter and Viewer<br><br>
-						<strong>Goal: </strong>Jessica likes looking at her friends’ photography.  She wants to be a photographer, but she doesn’t have a camera yet.  She uses other social media for sharing her own status and photos (from her phone), but uses jpegery to get inspiration from famous photographers.<br><br>
-						<strong>History: </strong>Jessica is in middle school; her life is consumed by her schoolwork but mostly by her friends and her social life.  Though she has access to a desktop computer, she accesses the Internet primarily via her smartphone.
-					</div>
-					<div class="col-md-6 personaborder">
-						<img src="../images/persona4.jpg" alt="Persona 4" class="img-circle img-responsive center-block" height="300" width="300"><br>
-						<p class="text-center"><strong>Chad Polumbaum, 47</strong><br>Artist</p><br>
-						<strong>User Type: </strong>Professional artist looking to advertise himself<br><br>
-						<strong>Goal: </strong>Chad is accomplished in digital media, abstract portraits in particular. He is semi-regularly asked to take commissions, and has been posting portraits (Which he created for practice, rather than profit) or other images on jpeger an average of once a month for the past two years.<br><br>
-						<strong>History: </strong>Chad is the father of two boys, ages ten and twelve. He left his job as a design consultant at Generic Inc. eleven years ago to raise his children, as his wife had a better salary and superior retirement benefits. He has strong opinions about politics.
-					</div>
-				</div>
-<br><br>
-		</div>
+
+			</div>
 	</body>
 </html>
