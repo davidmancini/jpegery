9e060ae9b1949a01d3e1cd5298ca0a941c7e639c
David Mancini <mancini.david@gmail.com>
2016-01-27 18:22:30 -0700

    Added __DIR__ to make head-tags.html work correctly on the server.

--- epic/index.php
+++ epic/index.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8"/>
 		<title>jpegery Epic</title>
 		<!--Include Bootstrap Head Tags -->
-		<? include_once ("../lib/head-tags.html"); ?>
+		<?php require_once(dirname(__DIR__) . "/lib/head-tags.html"); ?>
 		<link rel="stylesheet" href="../css/epicstyles.css">
 	</head>
 	<body>
@@ -24,12 +24,13 @@
 				<h3>Personas</h3><br>
 				<div class="row">
 					<div class="col-md-6 personaborder">
-						<img src="../images/persona1.jpg" alt="Persona 1" class="img-circle img-responsive center-block" height="300" width="300"><br>
-						<p class="text-center"><strong>Jason Holmes, 23</strong><br>Graduate Student, TA</p><br>
+						<div class="content-tile">
+							<img src="../images/persona1.jpg" alt="Persona 1" class="img-circle img-responsive center-block" height="300" width="300"><br>
+							<p class="text-center"><strong>Jason Holmes, 23</strong><br>Graduate Student, TA</p><br>
 							<strong>User Type: </strong>Non-professional, irregular poster.<br><br>
 							<strong>Goal: </strong>John has a casual interest in art, sketches of architecture in particular. He wishes to share his work for advice, inspiration, and praise. As such, he has posted content on jpegery three times, and is visiting the site to read any comments on his work.<br><br>
 							<strong>History: </strong>John was born into a middle-class family. He has one younger brother who is in his senior year of high-school. Both of his parents are employed. He majored in History two years ago and is currently working on his masters. He is somewhat bilingual, being relatively proficient with Russian. His parents probably thought they were very clever and ironic when they were naming him.
-
+						</div>
 
 					</div>
 					<div class="col-md-6 personaborder">
