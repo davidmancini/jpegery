47b2b6746323af4e60d8b2d09e8e875492db76f5
Jacob Findley <jfindley2@cnm.edu>
2016-01-28 13:10:06 -0700

    Updated user-stories

--- epic/user-stories.php
+++ epic/user-stories.php
@@ -12,14 +12,10 @@
 				"As a user, I want to upvote or downvote a user's image."<br/>
 				"As a user, I want to view the top images."<br/>
 				"As a user, I want to post images on the site."<br/>
+				“As a user, I want to see comments to my images on the site.”<br/>
 				"As a user, I want to create an account."<br/>
 				"As a user, I want to update my profile."<br/>
-				"As an administrator, I want to ban a user."<br/>
-				"As an administrator, I want to suspend a user."<br/>
-				"As an administrator, I want to warn a user."<br/>
-				"As an administrator, I want to update the site's news, such that I can communicate to users."<br/>
-				"As an administrator, I want to remove a user's image.<br/>
-				"As an administrator, I want to remove a user's comment."<br/>
+				“As a user, I want to flag an inappropriate image or comment.”<br/>
 			</p>
 		</div>
 	</body>
