a6f09c9ade71c8f8d2e32ad041641c67da32d6fe
Michael Kemm <mkemm1@cnm.edu>
2016-01-27 20:21:53 -0700

    Updated John Doe persona to Jason Holmes

---
+++ epic/personas/jasonHolmes.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Jason Holmes Persona</title>
+	</head>
+	<body>
+		<div>
+			<p><strong>Name:</strong> Jason Holmes</p>
+			<p><strong>Age:</strong> 23</p>
+			<p><strong>Job:</strong> Graduate Student, TA</p>
+			<p><strong>Goal:</strong> Though it’s not his area of study, Jason is serious about his photography, specifically his architecture photos.  He wishes to share his work for advice, inspiration, and praise. As such, he has posted content on jpegery on and off for the past few months, but is slowly taking the app more seriously so that he may be able to gather more exposure for his photography. </p>
+			<p><strong>History:</strong> <p>Jason was born into a middle-class family. He has one younger brother who is in his senior year of high-school. Both of his parents are employed. He majored in History two years ago and is currently working on his masters. He is somewhat bilingual, being relatively proficient with Russian. Jason has a Galaxy S5, but has never been one to enjoy using his smartphone. Recently the original Dell laptop he used for his undergrad had a motherboard failure. He currently uses the computers at school to edit the photos he takes on his Canon full-frame DSLR, but has purchased a chromebook for school work and web browsing as a stop-gap till he can afford a new ultrabook. </p>
+			<p><strong></strong></p>
+		</div>
+	</body>
+</html>
