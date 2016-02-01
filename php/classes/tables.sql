0c24dc5b20c580e5b68b320b8a17357f5a6cef37
David Mancini <mancini.david@gmail.com>
2016-02-01 12:37:44 -0700

    Added ; where one was missing.  Changed item.userId to item.profileId to be in line with rest of data design.

--- php/classes/tables.sql
+++ php/classes/tables.sql
@@ -13,16 +13,16 @@ CREATE TABLE profile (
 
 	email VARCHAR(128),
 	PRIMARY KEY(userId)
-)
+);
 
 CREATE TABLE item (
 	itemId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	userId INT UNSIGNED NOT NULL,
+	profileId INT UNSIGNED NOT NULL,
 	itemDescription VARCHAR(2000) NOT NULL,
 	images VARCHAR (64),
 	email VARCHAR (128) NOT NULL,
 	price INT UNSIGNED,
 	location VARCHAR (64) NOT NULL,
 	PRIMARY KEY(itemId),
-	FOREIGN KEY(userId) REFERENCES user(userId)
+	FOREIGN KEY(profileId) REFERENCES profile(profileId)
 );
