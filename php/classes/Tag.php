276211708d68cdbfb8dfcd61f3319af86ae5a7f2
zleyba <mtvzach@gmail.com>
2016-02-01 02:50:59 -0700

    finished the bulk of tag class

--- php/classes/Tag.php
+++ php/classes/Tag.php
@@ -36,7 +36,153 @@ class Tag {
 	 */
 
 	public function __construct(int $newTagId, string $newTagName) {
+
+		try {
+			$this->setTagId($newTagId);
+			$this->setTagName($newTagName);
+		}
+			catch(InvalidArgumentException $invalidArgument) {
+			// rethrow the exception to the caller
+			throw(new InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		}
+			catch(RangeException $range) {
+				// rethrow the exception to the caller
+				throw(new RangeException($range->getMessage(), 0, $range));
+			}
+			catch(Exception $exception) {
+				// rethrow generic exception
+				throw(new Exception($exception->getMessage(), 0, $exception));
+			}
+
 	}
 }
 
+/** accessor method for Tag ID
+ *
+ *@return int value of tag id
+ **/
 
+public function getTagId() {
+		return($this->tagId);
+}
+
+/**
+ * mutator method for tag id
+ *
+ * @param string $newTagId new value of tag id
+ * @throws \RangeException if $newTagId is not positive
+ * @throws \TypeError if $newTagId is not an integer
+ **/
+public function setTagId(int $newTagId) {
+	// verify the tag id is positive
+	if($newTagId <= 0) {
+		throw(new \RangeException("tag id is not positive"));
+	}
+
+	// convert and store the tag id
+	$this->tagId = $newTagId;
+}
+
+/** accessor method for tag name
+ *
+ *@return string value of tag name
+ **/
+
+public function getTagName() {
+	return($this->tagName);
+}
+
+/**
+ * mutator method for tag name
+ *
+ * @param string $newTagName new value of tag name
+ * @throws InvalidArgumentException if $newTag is not a string or insecure
+ * @throws RangeException if $newTag is > 64vcharacters
+ **/
+public function setTagName(int $newTagName) {
+	// verify the tag is secure
+	$newTagName = trim($newTagName);
+	$newTagName = filter_var($newTagName, FILTER_SANITIZE_STRING);
+	if(empty($newTagName) === true) {
+		throw(new InvalidArgumentException("Tag empty or insecure"));
+	}
+
+	// verify the email address will fit in the database
+	if(strlen($newTagName) > 64) {
+		throw(new RangeException("Tag too long"));
+	}
+
+	// store the email
+	$this->tagName = $newTagName;
+}
+
+/**
+ * inserts this Tag into mySQL
+ * @param PDO $pdo PDO Connection object
+ * @throws PDOException when mySQL related errors occur
+ */
+
+public function insert(PDO $pdo) {
+	//enformce the tagId is null (i.e. don't insert a tag that already exists)
+	if($this->tagId !=== null) {
+		throw(new PDOException ("not a new Tag"));
+	}
+
+	//create query template
+	$query = "INSERT INTO tag(tagId, tagName
+				VALUES(:tagId, :tagName))";
+				$statement = $pdo->prepare($query);
+
+	//update the null tagId with what mySQL just gave us
+	$this->tagId = intval($pdo->lastInsertId());
+
+
+
+}
+
+/**
+ * deletes this tag from mySQL
+ *
+ * @param PDO $pdo PDO connection object
+ * @throws PDOException when mySQL related errors occur
+ **/
+public function delete(PDO $pdo) {
+	// enforce the tagId is not null (i.e., don't delete a tag that hasn't been inserted)
+	if($this->tagId === null) {
+		throw(new PDOException("unable to delete a tag that does not exist"));
+	}
+
+	// create query template
+	$query	 = "DELETE FROM tag WHERE tagId = :tagId";
+	$statement = $pdo->prepare($query);
+
+	// bind the member variables to the place holder in the template
+	$parameters = array("tagId" => $this->tagId);
+	$statement->execute($parameters);
+}
+
+/**
+ * updates this tag in mySQL
+ *
+ * @param PDO $pdo PDO connection object
+ * @throws PDOException when mySQL related errors occur
+ **/
+public function update(PDO $pdo) {
+	// enforce the tagId is not null (i.e., don't update an tag that hasn't been inserted)
+	if($this->tagId === null) {
+		throw(new PDOException("unable to update a tag that does not exist"));
+	}
+
+	// create query template
+	$query	 = "UPDATE tag SET tagName :tagName,
+ 		WHERE tagId = :tagId";
+	$statement = $pdo->prepare($query);
+
+	// bind the member variables to the place holders in the template
+	$parameters = array("tagId" => $this->tagId, "tagName" => $this->tagName,
+	$statement->execute($parameters);
+}
+
+
+
+}
