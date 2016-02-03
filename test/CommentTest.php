569ca78633285fab4444352dfbe0a9135dc8b5ab
Jacob Findley <jfindley2@cnm.edu>
2016-02-03 09:59:35 -0700

    Added test cases to CommentTest.php https://app.asana.com/0/84113659891157/85573820735177

--- test/CommentTest.php
+++ test/CommentTest.php
@@ -49,19 +49,110 @@ class CommentTest extends JpegeryTest {
 		//Create and insert a Profile to post the test Comment
 		//TODO: Finish this.
 		$this->profile = new Profile(null, "John Public", "John", "867-5309");
+		$this->profile->insert($this->getPDO());
 
+		//TODO: Finish this.
+		$this->image = new Image(null, $this->profile->getProfileId());
+		//Calculate the date
+		$this->VALID_COMMENTDATE = new \DateTime();
 	}
 
+	/**
+	 * Test inserting a valid Comment and verify that the mySQL data matches
+	 *
+	 */
 	public function testInsertValidComment () {
 		//Count the number of rows for future use
 		$numRows = $this->getConnection()->getRowCount("comment");
 
 		//Create a new Comment and insert it into mySQL
-		$comment = new Comment(null, $this->image->getImageId(), $this->profile->getProfileId(), $this->VALID_COMMENTTEXT, $this->VALID_COMMENTDATE);
+		$comment = new Comment(null, $this->image->getImageId(), $this->profile->getProfileId(), $this->VALID_COMMENTDATE, $this->VALID_COMMENTTEXT);
 		$comment->insert($this->getPDO());
 
 		//Grab the data from mySQL and ensure that the fields match our expectations
 		$pdoComment = Comment::getCommentByCommentId($this->getPDO(), $comment->getCommentId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("comment"));
+		$this->assertEquals($pdoComment->getCommentImageId(), $this->image->getImageId());
+		$this->assertEquals($pdoComment->getCommentProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoComment->getCommentDate(), $this->VALID_COMMENTDATE);
+		$this->assertEquals($pdoComment->getCommentContent(), $this->VALID_COMMENTTEXT);
+	}
+
+	/**
+	 * Test inserting a comment that already exists
+	 */
+	public function testInsertInvalidComment() {
+		//Create a comment with a non null id and watch it fail
+		$comment = new Comment(JpegeryTest::INVALID_KEY, $this->image->getImageId(), $this->profile->getProfileId(), $this->VALID_COMMENTDATE, $this->VALID_COMMENTTEXT);
+		$comment->insert($this->getPDO());
+	}
+
+	/**
+	 * Test inserting a comment, editing it, and then updating it.
+	 */
+	public function testUpdateValidTweet() {
+		//Count the number of rows for later
+		$numRows = $this->getConnection()->getRowCount("comment");
+
+		//Create a new Comment and insert it into mySQL
+		$comment = new Comment(null, $this->image->getImageId(), $this->profile->getProfileId(), $this->VALID_COMMENTDATE, $this->VALID_COMMENTTEXT);
+		$comment->insert($this->getPDO());
+
+		//Edit the Comment and update it in mySQL
+		$comment->setCommentContent($this->VALID_COMMENTTEXT2);
+		$comment->update($this->getPDO());
+
+		//Grab the data from mySQL and check it against our expectations
+		$pdoComment = Comment::getCommentByCommentId($this->getPDO(), $comment->getCommentId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("comment"));
+		$this->assertEquals($pdoComment->getCommentImageId(), $this->image->getImageId());
+		$this->assertEquals($pdoComment->getCommentProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoComment->getCommentDate(), $this->VALID_COMMENTDATE);
+		$this->assertEquals($pdoComment->getCommentContent(), $this->VALID_COMMENTTEXT2);
+	}
+
+	/**
+	 * Test updating a Comment that already exists
+	 *
+	 * @expectedException \PDOException
+	 */
+	public function testUpdateInvalidComment() {
+		//Test updating a comment without inserting it
+		$comment = new Comment(null, $this->image->getImageId(), $this->profile->getProfileId(), $this->VALID_COMMENTDATE, $this->VALID_COMMENTTEXT);
+		$comment->update($this->getPDO());
+	}
+
+	/**
+	 * Test creating and deleting a comment.
+	 */
+	public function testDeleteValidComment() {
+		//Count the number of rows for later
+		$numRows = $this->getConnection()->getRowCount("comment");
+
+		//Create a new Comment and insert it into mySQL
+		$comment = new Comment(null, $this->image->getImageId(), $this->profile->getProfileId(), $this->VALID_COMMENTDATE, $this->VALID_COMMENTTEXT);
+		$comment->insert($this->getPDO());
+
+		$this->assertEquals($numRows+1, $this->getConnection()->getRowCount("comment"));
+		$comment->delete($this->getPDO());
+
+		//Grab the data from mySQL and ensure that the comment has been properly...dealt with.
+		$pdoComment = Comment::getCommentByCommentId($this->getPDO(), $comment->getCommentId());
+		$this->assertNull($pdoComment);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("comment"));
+	}
+
+	/**
+	 * Test deleting a Comment that does not exist
+	 *
+	 * @expectedException \PDOException
+	 */
+	public function testDeleteInvalidComment() {
+		//Create a comment and delete it without actually inserting it.
+		$comment = new Comment(null, $this->image->getImageId(), $this->profile->getProfileId(), $this->VALID_COMMENTDATE, $this->VALID_COMMENTTEXT);
+		$comment->delete($this->getPDO());
+	}
+	public function testGetValidCommentByCommentId() {
 
 	}
 }
