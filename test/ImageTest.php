4bf72c0a20a1958d174f92f404eefee4225a4bb5
David Mancini <mancini.david@gmail.com>
2016-02-03 16:02:55 -0700

    Continued to work on ImageTest
    https://app.asana.com/0/84113659891181/85573862538630

--- test/ImageTest.php
+++ test/ImageTest.php
@@ -27,19 +27,25 @@ class ImageTest extends JpegeryTest {
 	 * String of image file name
 	 * @var String $VALID_IMAGEFILENAME
 	 */
-	protected $VALID_IMAGEFILENAME = null;
+	protected $VALID_IMAGEFILENAME = "image.jpg";
 
 	/*
 	 * String of image text
 	 * @var String $VALID_IMAGETEXT
 	 */
-	protected $VALID_IMAGETEXT = null;
+	protected $VALID_IMAGETEXT = "This is an image!";
+
+	/*
+	 * String of updated image text
+	 * @var String $VALID_IMAGETEXT2
+	 */
+	protected $VALID_IMAGETEXT2 = "This is updated image text."
 
 	/*
  * String of image type
  * @var String $VALID_IMAGETYPE
  */
-	protected $VALID_IMAGETYPE = null;
+	protected $VALID_IMAGETYPE = "jpg";
 
 	/*
 	 * Create dependent objects before running each test
@@ -50,7 +56,7 @@ class ImageTest extends JpegeryTest {
 
 		//Create and insert a profile to own the test image
 		//profileId, profileAdmin, profileCreateDate, profileEmail, profileHandle, profileHash, profileImageId, profileName, profilePhone, profileSalt, profileVerify
-		$this->profile = new Profile(null, null, null, "test@example.com", "testGuy", null, null, "Test", "Guy", "800-555-1234", null, "true");
+		$this->profile = new Profile(null, false, null, "test@example.com", "testGuy", null, null, "Test Guy", "800-555-1234", null, "true");
 		$this->profile->insert($this->getPDO());
 
 		//Calculate the date that was just set up
@@ -67,9 +73,53 @@ class ImageTest extends JpegeryTest {
 		//Create a new Image and insert into MySQL
 		//newImageId, newProfileId, newImageType, newImageFileName, newImageText, newImageDate
 		$image = new Image (null, $this->profile->getProfileId(), $this->VALID_IMAGETYPE, $this->VALID_IMAGETEXT, $this->VALID_IMAGEDATE);
+		$image->insert($this->getPDO());
 
-
+		//Grab data from MySQL and enforce fields to match expectations
+		$pdoImage = Image::getImageByImageId($this->getPDO(), $image->getImageId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("image"));
+		$this->assertEquals($pdoImage->getImageProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoImage->getImageType(), $this->VALID_IMAGETYPE);
+		$this->assertEquals($pdoImage->getImageText(), $this->VALID_IMAGETEXT);
+		$this->assertEquals($pdoImage->getImageDate(), $this->VALID_IMAGEDATE);
 	}
+
+	/*
+	 * Test inserting an image that already exists
+	 *
+	 * @expectedException PDOException
+	 */
+	public function testInsertInvalidImage() {
+		//Create an image with a non-null image id and watch it fail
+		$image = new Image(JpegeryTest::INVALID_KEY, $this->profile->getProfileId(), $this->VALID_IMAGETYPE, $this->VALID_IMAGETEXT, $this->-$this->VALID_IMAGEDATE);
+		$image->insert($this->getPDO());
+	}
+
+	/*
+	 * Test inserting an image, editing it, then updating it
+	 */
+
+	public function testUpdateValidImage() {
+		//Count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("image");
+
+		//Create a new image and insert it into MySQL
+		$image = new Image(null, $this->profile->getProfileId(), $this->VALID_IMAGETYPE, $this->VALID_IMAGETEXT, $this->VALID_IMAGEDATE);
+		$image->insert($this->getPDO());
+
+		//Edit the image and update it
+		$image->setImageText($this->VALID_IMAGETEXT2);
+		$image->update($this->getPDO());
+
+		//Grab data from MySQL and ensure fields match expectations
+		$pdoImage = Image::getImageByImageId($this->getPDO(), $image->getImageId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("image"));
+		$this->assertEquals($pdoImage->getImageProfileId(), $this->profile->getProfileId);
+		$this->assertEquals($pdoImage->getImageType(), $this->VALID_IMAGETYPE);
+		$this->assertEquals($pdoImage->getImageText(), $this->VALID_IMAGETEXT2);
+		$this->assertEquals($pdoImage->getImageDate(), $this->VALID_IMAGEDATE);
+	}
+
 
 
 
