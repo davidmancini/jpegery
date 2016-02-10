de27237eafe970b3e338d418a05155736e902966
zleyba <mtvzach@gmail.com>
2016-02-09 23:31:04 -0700

    began unit test build for tag https://app.asana.com/0/84113647261173/85574344478343/f

---
+++ test/TagTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This is a complete PHPUnit test for the Tag class of the Jpegery app
+ *It is complete because *ALL* mySQL/PDO enabled methods are tested for both invalid and valid inputs.
+ *
+ * @see Tag
+ * @authors David Mancini, Jacob Findley, Michael Kemm, Zach Leyba
+ **/
+
+namespace Edu\Cnm\Jpegery\Test;
+
+use Edu
