f7d1b5825f9212ca9634225e1479190dfe35e886
zleyba <mtvzach@gmail.com>
2016-02-09 23:45:06 -0700

    phase one of tag test build https://app.asana.com/0/84113647261173/85574344478343/f

--- test/TagTest.php
+++ test/TagTest.php
@@ -1,4 +1,17 @@
 <?php
+
+namespace Edu\Cnm\Jpegery\Test;
+
+use Edu\Cnm\Jpegery\{
+	Tag
+};
+
+//grab the project test parameters
+require_once("JpgeryTest.php");
+
+//grab the class under scrutiny
+require_once(dirname(__DIR__) . "/php/classes/autoload.php");
+
 /**
  * This is a complete PHPUnit test for the Tag class of the Jpegery app
  *It is complete because *ALL* mySQL/PDO enabled methods are tested for both invalid and valid inputs.
@@ -6,7 +19,12 @@
  * @see Tag
  * @authors David Mancini, Jacob Findley, Michael Kemm, Zach Leyba
  **/
+class TagTest extends JpegeryTest {
+	/**
+	 * tag id
+	 * @var string $VALID_TAGNAME
+	 **/
+	protected $VALID_TAGNAME = "PHPUnitTestPass";
 
-namespace Edu\Cnm\Jpegery\Test;
 
-use Edu
+}
