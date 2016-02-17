eef19f60ac0c071b48f04c11f0a4201ec7bcc966
David Mancini <mancini.david@gmail.com>
2016-02-17 15:35:33 -0700

    Added lib/xsrf.php and api/image/index.php https://app.asana.com/0/84113659891181/91309023675428

---
+++ php/api/image/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once dirname(dirname(__DIR__)) . "/classes/autoload.php";
+require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
