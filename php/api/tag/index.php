7e63f05d18345620473a3ec6774815e5c379784a
zleyba <mtvzach@gmail.com>
2016-02-19 08:46:49 -0700

    added xsrf challenege https://app.asana.com/0/84113659891097/91309023675432

--- php/api/tag/index.php
+++ php/api/tag/index.php
@@ -5,5 +5,23 @@
  * @author Zach Leyba mtvzach@gmail.com
  */
 
-//verify the zsrf challenge
-if(session_status())
+//verify the xsrf challenge
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an emptry reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("etc/apache2/capstone-mysql/jpegerey.ini");
+	// if the tag session is empty, the user is not logged in, throw an exception
+	if(empty(_$SESSION["Tag"]) === true) {
+		setXsrfCookie('/');
+		throw new (new RuntimeException("You Must Sign In to Tag a Photo", 401));
+	}
+
+}
