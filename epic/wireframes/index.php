bc3cfa44a2c511ee8d7c7bd3761102284a0f6261
David Mancini <mancini.david@gmail.com>
2016-02-19 09:25:11 -0700

    Added Settings wireframes https://app.asana.com/0/84113659891181/91309023675436

--- epic/wireframes/index.php
+++ epic/wireframes/index.php
@@ -30,31 +30,39 @@
 				<h1 class="page-header">Wireframes</h1>
 
 				<div class="row row-eq-height">
-					<div class="col-md-4 personaborder">
+					<div class="col-md-3 personaborder">
 						<a href="desktop-home.php"><img src="thumbnail-desktop-home.png" alt="Home page, Desktop" class="img-responsive"></a><br>
 						Home Page, Desktop
 					</div>
-					<div class="col-md-4 personaborder">
+					<div class="col-md-3 personaborder">
 						<a href="desktop-image.php"><img src="thumbnail-desktop-image.png" alt="Single Image, Desktop" class="img-responsive"></a><br>
 						Single Image, Desktop
 					</div>
-					<div class="col-md-4 personaborder">
+					<div class="col-md-3 personaborder">
 						<a href="desktop-user.php"><img src="thumbnail-desktop-user.png" alt="User Profile, Desktop" class="img-responsive"></a><br>
 						User Profile, Desktop
+					</div>
+					<div class="col-md-3 personaborder">
+						<a href="desktop-settings.php"><img src="thumbnail-desktop-settings.png" alt="User Settings, Desktop" class="img-responsive"></a><br>
+						Home Page, Desktop
 					</div>
 				</div>
 
 				<div class="row row-eq-height">
-					<div class="col-md-4 personaborder">
+					<div class="col-md-3 personaborder">
 						<a href="mobile-home.php"><img src="thumbnail-mobile-home.png" alt="Home page, Mobile" class="img-responsive"></a><br>
 						Home Page, Mobile
 					</div>
-					<div class="col-md-4 personaborder">
+					<div class="col-md-3 personaborder">
 						<a href="mobile-image.php"><img src="thumbnail-mobile-image.png" alt="Single Image, Mobile" class="img-responsive"></a><br>
 						Single Image, Mobile
 					</div>
-					<div class="col-md-4 personaborder">
+					<div class="col-md-3 personaborder">
 						<a href="mobile-user.php"><img src="thumbnail-mobile-user.png" alt="User Profile, Mobile" class="img-responsive"></a><br>
+						User Profile, Desktop
+					</div>
+					<div class="col-md-3 personaborder">
+						<a href="mobile-settings.php"><img src="thumbnail-mobile-settings.png" alt="User Settings, Mobile" class="img-responsive"></a><br>
 						User Profile, Desktop
 					</div>
 				</div>
