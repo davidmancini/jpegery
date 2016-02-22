70e5131691aa62f5a04fc7907eb9c9a44d8a572e
David Mancini <mancini.david@gmail.com>
2016-02-22 11:54:07 -0700

    Edited footer with filler URLs. https://app.asana.com/0/84113659891181/91309023675428

--- footer.php
+++ footer.php
@@ -1,5 +1,15 @@
 <div class="container-fluid footer">
 	<div class="row">
+		<div class="col-xs-6">
+			<a href="#">Log In</a><br>
+			<a href="#">Help</a><br>
+			<a href="#">FAQ</a><br>
+		</div>
+		<div class="col-xs-6 text-right">
+			<a href="#">Contact Us</a>
+		</div>
+	</div>
+	<div class="row">
 		<div class="col-md-12">
 			<p class="text-center">Copyright &copy; 2015-<?php echo date("Y");?>&nbsp;jpegery<br>All Rights Reserved.</p>
 		</div>
