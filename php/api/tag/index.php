6adf059dae5befb058f8a0cb4d0b306cac3479ed
zleyba <mtvzach@gmail.com>
2016-02-21 20:43:55 -0700

    added get tag by name https://app.asana.com/0/84113659891097/91309023675432

--- php/api/tag/index.php
+++ php/api/tag/index.php
@@ -29,12 +29,36 @@ try {
 //determine which HTTP method was used
 $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
-//sanatize inputs and trim other fields
+//sanatize inputs
 $id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 
 //make sure the ID is valid is valid for methods that require it
 if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
 	throw(new InvalidArgumentException("ID cannot be empty or negative"));
+}
+
+//sanitize and trim other fields
+$tagId = filter_input(INPUT_GET, "tagId", FILTER_VALIDATE_INT);
+$tagName = filter_input(INPUT_GET, "tagName", FILTER_SANITIZE_STRING);
+
+//handle REST calls, while only allowing admin to access database-modifying methods
+if($method === "GET") {
+	//set XSFR cookie
+	setXsrfCookie("/");
+	//get tag based on the given field
+	if(empty($id) === false) {
+		$tag = Tag::getTagById($pdo, $id);
+		if($tag !== null && $tag->getTagId() === $_SESSION["tag"]->getTagId()) {
+			$reply->data = $tag;
+		}
+	} else if(empty($tagName) === false) {
+		$tag = Tag::getTagByName($pdo, $id);
+		if($tag !== null && $tag->getTagName() === $_SESSION["tag"]->getTagName()) {
+			$reply->data = $tag;
+		}
+
+	}
+
 }
 
 
