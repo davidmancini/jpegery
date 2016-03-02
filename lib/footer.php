f03ebbe5dea0076983188f780039632b8f5cf667
David Mancini <mancini.david@gmail.com>
2016-03-02 16:47:01 -0700

    Changed footer links to one line. https://app.asana.com/0/84113659891181/91309023675436

--- lib/footer.php
+++ lib/footer.php
@@ -1,9 +1,9 @@
 <div class="container-fluid footer">
 	<div class="row">
 		<div class="col-xs-6">
-			<a href="#"><i class="fa fa-sign-in"></i>&nbsp;Log In</a><br>
-			<a href="#"><i class="fa fa-info"></i>&nbsp;Help</a><br>
-			<a href="#"><i class="fa fa-question"></i>&nbsp;FAQ</a><br>
+			<a href="#"><i class="fa fa-sign-in"></i>&nbsp;Log In</a>&nbsp;&nbsp;
+			<a href="#"><i class="fa fa-info"></i>&nbsp;Help</a>&nbsp;&nbsp;
+			<a href="#"><i class="fa fa-question"></i>&nbsp;FAQ</a>
 		</div>
 		<div class="col-xs-6 text-right">
 			<a href="#"><i class="fa fa-envelope-o"></i>&nbsp;Contact Us</a>
