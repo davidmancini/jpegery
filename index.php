39f689f239d5fde5e0c5ea00e2e10ff721959cd5
David Mancini <mancini.david@gmail.com>
2016-03-02 14:05:19 -0700

    Removed extra placeholder text. https://app.asana.com/0/84113659891181/91309023675436

--- index.php
+++ index.php
@@ -3,88 +3,98 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Jpegery</title>
-		<?php require_once("lib/head-tags.html")?><!--Includes Bootstrap, Google Fonts, Font Awesome, etc. -->
+		<?php require_once("lib/head-tags.html") ?><!--Includes Bootstrap, Google Fonts, Font Awesome, etc. -->
 		<link rel="stylesheet" href="css/style.css"><!--Overrides core Bootstrap-->
 	</head>
 	<body>
 		<div class="mainContent">
 			<header>
-				<?php require_once ("lib/header.php"); ?>
+				<?php require_once("lib/header.php"); ?>
 			</header>
 
 			<main>
 				<div class="container contentContainer">
-						<div class="row">
-							<div class="col-md-6 text-center">
-								Login/Registation Form<br><br><br><br><br><br>
+					<div class="row">
+						<div class="col-md-6 text-center">
+							<!--Blank section. Could be used for company banner?-->
+						</div>
+						<div class="col-md-6 text-center">
+							<div class="input-group search hidden-sm hidden-md hidden-lg">
+								<input type="text" class="form-control" name="search" id="search" placeholder="Search for...">
+									<span class="input-group-btn">
+										<button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+									</span>
 							</div>
-							<div class="col-md-6 text-center">
-								<div class="input-group search hidden-sm hidden-md hidden-lg">
-									<input type="text" class="form-control" name="search" id="search" placeholder="Search for...">
-										<span class="input-group-btn">
-											<button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
-										</span>
+							<div class="btn-group" role="group" aria-label="...">
+								<button type="button" class="btn btn-default">Following</button>
+								<button type="button" class="btn btn-default">Followers</button>
+								<button type="button" class="btn btn-default">Trending</button>
+								<button type="button" class="btn btn-default">Most Popular</button>
+								<div class="btn-group" role="group">
+									<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">View By&nbsp;<span class="caret"></span></button>
+									<ul class="dropdown-menu">
+										<li><a href="#">Not Yet Followed</a></li>
+										<li><a href="#">Other...</a></li>
+									</ul>
 								</div>
-
-								<div class="btn-group" role="group" aria-label="...">
-									<button type="button" class="btn btn-default">Following</button>
-									<button type="button" class="btn btn-default">Followers</button>
-									<button type="button" class="btn btn-default">Trending</button>
-									<button type="button" class="btn btn-default">Most Popular</button>
-									<div class="btn-group" role="group">
-										<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">View By&nbsp;<span class="caret"></span></button>
-										<ul class="dropdown-menu">
-											<li><a href="#">Not Yet Followed</a></li>
-											<li><a href="#">Other...</a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
+							</div><!--End Button Group-->
+						</div>
 					</div>
 
 					<div class="row contentRow contentImageRow">
 						<div class="col-xs-6 col-sm-4 col-md-3">
-							<a href="#"><img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage"></a>
+							<a href="#"><img src="images/placeholder.png" alt="Placeholder image"
+												  class="img-responsive contentImage"></a>
 						</div>
 						<div class="col-xs-6 col-sm-4 col-md-3">
-							<a href="#"><img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage"></a>
+							<a href="#"><img src="images/placeholder.png" alt="Placeholder image"
+												  class="img-responsive contentImage"></a>
 						</div>
 						<div class="col-xs-6 col-sm-4 col-md-3">
-							<a href="#"><img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage"></a>
+							<a href="#"><img src="images/placeholder.png" alt="Placeholder image"
+												  class="img-responsive contentImage"></a>
 						</div>
 						<div class="col-xs-6 col-sm-4 col-md-3">
-							<a href="#"><img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage"></a>
+							<a href="#"><img src="images/placeholder.png" alt="Placeholder image"
+												  class="img-responsive contentImage"></a>
 						</div>
-<!--					</div>-->
-<!--					<div class="row contentRow">-->
+						<!--					</div>-->
+						<!--					<div class="row contentRow">-->
 						<div class="col-xs-6 col-sm-4 col-md-3">
-							<a href="#"><img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage"></a>
+							<a href="#"><img src="images/placeholder.png" alt="Placeholder image"
+												  class="img-responsive contentImage"></a>
 						</div>
 						<div class="col-xs-6 col-sm-4 col-md-3">
 							<img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage">
 						</div>
 						<div class="col-xs-6 col-sm-4 col-md-3">
-							<a href="#"><img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage"></a>
+							<a href="#"><img src="images/placeholder.png" alt="Placeholder image"
+												  class="img-responsive contentImage"></a>
 						</div>
 						<div class="col-xs-6 col-sm-4 col-md-3">
-							<a href="#"><img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage"></a>
+							<a href="#"><img src="images/placeholder.png" alt="Placeholder image"
+												  class="img-responsive contentImage"></a>
 						</div>
-<!--					</div>-->
-<!--					<div class="row contentRow">-->
+						<!--					</div>-->
+						<!--					<div class="row contentRow">-->
 						<div class="col-xs-6 col-sm-4 col-md-3">
-							<a href="#"><img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage"></a>
-						</div>
-						<div class="col-xs-6 col-sm-4 col-md-3">
-							<a href="#"><img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage"></a>
-						</div>
-						<div class="col-xs-6 col-sm-4 col-md-3">
-							<a href="#"><img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage"></a>
+							<a href="#"><img src="images/placeholder.png" alt="Placeholder image"
+												  class="img-responsive contentImage"></a>
 						</div>
 						<div class="col-xs-6 col-sm-4 col-md-3">
-							<a href="#"><img src="images/placeholder.png" alt="Placeholder image" class="img-responsive contentImage"></a>
+							<a href="#"><img src="images/placeholder.png" alt="Placeholder image"
+												  class="img-responsive contentImage"></a>
 						</div>
-<!--					</div>-->
-				</div><!--/contentContainer-->
+						<div class="col-xs-6 col-sm-4 col-md-3">
+							<a href="#"><img src="images/placeholder.png" alt="Placeholder image"
+												  class="img-responsive contentImage"></a>
+						</div>
+						<div class="col-xs-6 col-sm-4 col-md-3">
+							<a href="#"><img src="images/placeholder.png" alt="Placeholder image"
+												  class="img-responsive contentImage"></a>
+						</div>
+						<!--					</div>-->
+					</div><!--/contentContainer-->
 			</main>
 		</div><!--/mainContent-->
 		<footer>
