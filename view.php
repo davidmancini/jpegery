091f76965b54c2b5638a2bb6a22e7881d0ca3deb
David Mancini <mancini.david@gmail.com>
2016-03-08 10:32:05 -0700

    Created a temporary index page to view scaffolding files. https://app.asana.com/0/84113659891181/91309023675436

---
+++ view.php
@@ -0,0 +1,15 @@
+<!--Temporary file to be used as an index to view scaffolding pages.-->
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>View jpegery Pages</title>
+	</head>
+	<body>
+		<p><a href="index.php">Index</a></p>
+		<p><a href="image.php">Single Image</a></p>
+		<p><a href="user.php">User Page</a></p>
+		<p><a href="settings.php">Settings</a></p>
+	</body>
+</html>
