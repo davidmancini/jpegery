a230a6dcb3ac241dc777d3ca636edee70bc5be7a
David Mancini <mancini.david@gmail.com>
2016-03-11 09:30:51 -0700

    Created super simple login form. https://app.asana.com/0/84113659891181/91309023675436

---
+++ login.php
@@ -0,0 +1,5 @@
+<form action="">
+	Username: <input type="text" name="emailHandlePhone" id="emailHandlePhone"><br>
+	Password: <input type="password" name="password" id="password">
+	<button type="submit">Log In</button>
+</form>
