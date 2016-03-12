601cd5bb43ddaf40d0da5e2575df31007f51b7d2
David Mancini <mancini.david@gmail.com>
2016-03-12 09:18:38 -0700

    Working on things https://app.asana.com/0/84113659891181/91309023675436

--- angular/controllers/settings-controller.js
+++ angular/controllers/settings-controller.js
@@ -1,5 +1,19 @@
-app.controller('settingsController', function($scope) {
+app.controller('settingsController', ["$scope", "profileService", function($scope, profileService) {
 
+	$scope.profile = {};
+	$scope.alerts = [];
 
+	$scope.getCurrentProfile = function() {
+		profileService.fetchCurrent(true)
+			.then(function(result) {
+				if (result.data.status === 200) {
+					$scope.profile = result.data.data;
+				} else {
+					$scope.alerts[0] = {
+						type: "danger",
+						msg: "You are not logged in."
+				}
+			});
+	};
 
-});
+}]);
