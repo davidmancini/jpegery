3e53dc9f18aaddc3b91abcf47d6e084a21bdb829
David Mancini <mancini.david@gmail.com>
2016-03-15 09:18:09 -0600

    Re-routed view.php helper file for Angular Route pages. https://app.asana.com/0/84113659891181/91309023675436

--- view.php
+++ view.php
@@ -7,10 +7,12 @@
 		<title>View jpegery Pages</title>
 	</head>
 	<body>
-		<p><a href="index.php">Index</a></p>
-		<p><a href="image.php">Single Image</a></p>
-		<p><a href="user.php">User Page</a></p>
-		<p><a href="settings.php">Settings</a></p>
-		<p><a href="upload.php">Upload Image</a></p>
+		<p><a href="index">Index</a></p>
+		<p><a href="image">Single Image</a></p>
+		<p><a href="user">User Page</a></p>
+		<p><a href="settings">Settings</a></p>
+		<p><a href="upload">Upload Image</a></p>
+		<p><a href="register">Register</a></p>
+		<p><a href="login">Log In</a></p>
 	</body>
 </html>
