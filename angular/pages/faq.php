7164496572fc9b958beaa240953d408820e8eea8
Michael Kemm <mkemm1@cnm.edu>
2016-03-15 17:11:12 -0600

    modified faq

--- angular/pages/faq.php
+++ angular/pages/faq.php
@@ -1,22 +1,25 @@
 <div class="container">
-	<br />
-	<br />
-	<br />
+	<br/>
+	<br/>
+	<br/>
 
 	<div class="alert alert-warning alert-dismissible" role="alert">
-		<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-		<p>This section contains a wealth of information, related to <strong>jpegery</strong>. If you cannot find an answer to your question,
-		make sure to contact us.</P>
+		<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span
+				class="sr-only">Close</span></button>
+		<p>This section contains a wealth of information, related to <strong>jpegery</strong>. If you cannot find an
+			answer to your question,
+			make sure to contact us.</P>
 	</div>
 
-	<br />
+	<br/>
 
 	<div class="panel-group" id="accordion">
 		<div class="faqHeader">General questions</div>
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h4 class="panel-title">
-					<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Is account registration required?</a>
+					<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Is
+						account registration required?</a>
 				</h4>
 			</div>
 			<div id="collapseOne" class="panel-collapse collapse in">
@@ -28,19 +31,22 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h4 class="panel-title">
-					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTen">Can I submit my own images?</a>
+					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
+						href="#collapseTen">Can I submit my own images?</a>
 				</h4>
 			</div>
 			<div id="collapseTen" class="panel-collapse collapse">
 				<div class="panel-body">
-					<p>All of the content of the site has been submitted by the community. You are encouraged to contribute.</p>
+					<p>All of the content of the site has been submitted by the community. You are encouraged to
+						contribute.</p>
 				</div>
 			</div>
 		</div>
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h4 class="panel-title">
-					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEleven">How do I get started?</a>
+					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
+						href="#collapseEleven">How do I get started?</a>
 				</h4>
 			</div>
 			<div id="collapseEleven" class="panel-collapse collapse">
@@ -60,7 +66,8 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h4 class="panel-title">
-					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Who are you guys?</a>
+					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
+						href="#collapseTwo">Who are you guys?</a>
 				</h4>
 			</div>
 			<div id="collapseTwo" class="panel-collapse collapse">
@@ -72,7 +79,8 @@
 		<div class="panel panel-default fa-arrow-down">
 			<div class="panel-heading">
 				<h4 class="panel-title">
-					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">Why Images?</a>
+					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
+						href="#collapseThree">Why Images?</a>
 				</h4>
 			</div>
 			<div id="collapseThree" class="panel-collapse collapse">
@@ -84,20 +92,22 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h4 class="panel-title">
-					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive">How much do I get from each sale?</a>
+					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
+						href="#collapseFive">How much do I get from each sale?</a>
 				</h4>
 			</div>
 			<div id="collapseFive" class="panel-collapse collapse">
 				<div class="panel-body">
 					<p>We think you're using the wrong site...</p>
-					<br />
+					<br/>
 				</div>
 			</div>
 		</div>
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h4 class="panel-title">
-					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix">You guys seem awesome, but are you really?</a>
+					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
+						href="#collapseSix">You guys seem awesome, but are you really?</a>
 				</h4>
 			</div>
 			<div id="collapseSix" class="panel-collapse collapse">
@@ -111,7 +121,8 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h4 class="panel-title">
-					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">I really want to use your site, but I'm scared.</a>
+					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
+						href="#collapseFour">I really want to use your site, but I'm scared.</a>
 				</h4>
 			</div>
 			<div id="collapseFour" class="panel-collapse collapse">
@@ -123,7 +134,8 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h4 class="panel-title">
-					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven">jpegery seems cool, but I want more features.</a>
+					<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
+						href="#collapseSeven">jpegery seems cool, but I want more features.</a>
 				</h4>
 			</div>
 			<div id="collapseSeven" class="panel-collapse collapse">
@@ -135,34 +147,34 @@
 	</div>
 </div>
 
-<!--<style>-->
-<!--	.faqHeader {-->
-<!--		font-size: 27px;-->
-<!--		margin: 20px;-->
-<!--	}-->
-<!---->
-<!--	.panel-heading [data-toggle="collapse"]:after {-->
-<!--		font-family: 'Glyphicons Halflings';-->
-<!--		content: "e072"; /* "play" icon */-->
-<!--		float: right;-->
-<!--		color: #F58723;-->
-<!--		font-size: 18px;-->
-<!--		line-height: 22px;-->
-<!--		/* rotate "play" icon from > (right arrow) to down arrow */-->
-<!--		-webkit-transform: rotate(-90deg);-->
-<!--		-moz-transform: rotate(-90deg);-->
-<!--		-ms-transform: rotate(-90deg);-->
-<!--		-o-transform: rotate(-90deg);-->
-<!--		transform: rotate(-90deg);-->
-<!--	}-->
-<!---->
-<!--	.panel-heading [data-toggle="collapse"].collapsed:after {-->
-<!--		/* rotate "play" icon from > (right arrow) to ^ (up arrow) */-->
-<!--		-webkit-transform: rotate(90deg);-->
-<!--		-moz-transform: rotate(90deg);-->
-<!--		-ms-transform: rotate(90deg);-->
-<!--		-o-transform: rotate(90deg);-->
-<!--		transform: rotate(90deg);-->
-<!--		color: #454444;-->
-<!--	}-->
-<!--/*</style>*/-->
+<style>
+	.faqHeader {
+		font-size: 27px;
+		margin: 20px;
+	}
+
+	.panel-heading [data-toggle="collapse"]:after {
+		font-family: 'Glyphicons Halflings';
+		content: "e072"; /* "play" icon */
+		float: right;
+		color: #F58723;
+		font-size: 18px;
+		line-height: 22px;
+		/* rotate "play" icon from > (right arrow) to down arrow */
+		-webkit-transform: rotate(-90deg);
+		-moz-transform: rotate(-90deg);
+		-ms-transform: rotate(-90deg);
+		-o-transform: rotate(-90deg);
+		transform: rotate(-90deg);
+	}
+
+	.panel-heading [data-toggle="collapse"].collapsed:after {
+		/* rotate "play" icon from > (right arrow) to ^ (up arrow) */
+		-webkit-transform: rotate(90deg);
+		-moz-transform: rotate(90deg);
+		-ms-transform: rotate(90deg);
+		-o-transform: rotate(90deg);
+		transform: rotate(90deg);
+		color: #454444;
+	}
+</style>
--- php/api/logout/index.php
+++ php/api/logout/index.php
@@ -1,4 +1,5 @@
 <?php
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
-}$_SESSION = [];
+}
+$_SESSION = [];
