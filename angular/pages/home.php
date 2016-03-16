2a283c1835f667d89338a27fdfe01033c8447d2b
Michael Kemm <mkemm1@cnm.edu>
2016-03-16 15:23:30 -0600

    removed filter by until further notice

--- angular/pages/home.php
+++ angular/pages/home.php
@@ -1,19 +1,18 @@
 <div class="row aboveRow">
-	<ng-include src="'angular/templates/content-search.php'"></ng-include>
-	<div class="btn-group pull-right" role="group" aria-label="...">
-		<button type="button" class="btn btn-default"><i class="fa fa-users"></i>&nbsp;Following</button>
-		<button type="button" class="btn btn-default"><i class="fa fa-exchange"></i>&nbsp;Followers</button>
-		<button type="button" class="btn btn-default"><i class="fa fa-fire"></i>&nbsp;Trending</button>
-		<button type="button" class="btn btn-default"><i class="fa fa-trophy"></i>&nbsp;Most Popular</button>
-		<div class="btn-group" role="group">
-			<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-					  aria-expanded="false">View By&nbsp;<span class="caret"></span></button>
-			<ul class="dropdown-menu">
-				<li><a href="#">Not Yet Followed</a></li>
-				<li><a href="#">Other...</a></li>
-			</ul>
-		</div>
-	</div><!--End Button Group-->
+	<ng-include src="'angular/templates/content-search.php'"></ng-include><div class="btn-group pull-right" role="group" aria-label="...">
+<!--		<button type="button" class="btn btn-default"><i class="fa fa-users"></i>&nbsp;Following</button>-->
+<!--		<button type="button" class="btn btn-default"><i class="fa fa-exchange"></i>&nbsp;Followers</button>-->
+<!--		<button type="button" class="btn btn-default"><i class="fa fa-fire"></i>&nbsp;Trending</button>-->
+<!--		<button type="button" class="btn btn-default"><i class="fa fa-trophy"></i>&nbsp;Most Popular</button>-->
+<!--		<div class="btn-group" role="group">-->
+<!--			<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"-->
+<!--					  aria-expanded="false">View By&nbsp;<span class="caret"></span></button>-->
+<!--			<ul class="dropdown-menu">-->
+<!--				<li><a href="#">Not Yet Followed</a></li>-->
+<!--				<li><a href="#">Other...</a></li>-->
+<!--			</ul>-->
+<!--		</div>-->
+<!--	</div><!--End Button Group-->-->
 	<br/>
 </div>
 
